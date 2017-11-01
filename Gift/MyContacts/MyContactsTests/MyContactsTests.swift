80b4fd03e83a4ffe626273f9fa44b00cc5d59388
Koketso42 <KoketsoMat@nedbank.co.za>
2017-11-01 12:08:18 +0200

    Initial TDD

--- Gift/MyContacts/MyContactsTests/MyContactsTests.swift
+++ Gift/MyContacts/MyContactsTests/MyContactsTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import ContactEntry
 @testable import MyContacts
 
 class MyContactsTests: XCTestCase {
@@ -31,6 +32,21 @@ class MyContactsTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+
+    func shouldInitialiseContactEntryObject() {
+        
+        let contactEntry: ContactEntry? = ContactEntry();
+
+        XCTAssert(contactEntry.isKindOfClass(ContactEntry)) // should instantiate ContactEntry
+
+    }   
+    
+    func shouldHaveFullName() {
+
+        let contactEntry: NSObject?
+
+
     }
     
 }
